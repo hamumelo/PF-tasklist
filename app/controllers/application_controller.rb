c2a37eadf62a7321dfc1b6fe9d0938afe0d154ac
hamumelo <namahamu0920@gmail.com>
2022-03-20 03:06:17 +0000

    update220320-8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def after_sign_in_path_for(resource)
-  #   case resource
-  #   when User
-  #     root_path
-  #   when Admin
-  #     admin_path
-  #   end
-  # end
+  def after_sign_in_path_for(resource)
+    case resource
+    when User
+      root_path
+    when Admin
+      admin_users_path
+    end
+  end
 
   protected
 
