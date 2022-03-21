649131144a5ad95a008e2199cabb0e222875edd0
hamumelo <namahamu0920@gmail.com>
2022-03-21 11:36:35 +0000

    update220321-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-        # has_many :events, dependent: :destroy
+        has_many :events, dependent: :destroy
 
 
   validates :email, presence: true
